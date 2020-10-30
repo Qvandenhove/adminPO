4b309ea817364e6cedc1c6676f7b7f13ccd25f48
QVandenhove <quentin.vandenhove@viacesi.fr>
2020-10-30 14:25:53 +0100

    Recherche de formation ajoutée

---
+++ src/helpers/helpers.tsx
@@ -0,0 +1,32 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAr2ARyw2PBu7i9l_dvRAgl3X4cgE0Voi4",
+    authDomain: "satisfaction-7bbc4.firebaseapp.com",
+    databaseURL: "https://satisfaction-7bbc4.firebaseio.com",
+    projectId: "satisfaction-7bbc4",
+    storageBucket: "satisfaction-7bbc4.appspot.com",
+    messagingSenderId: "88882724595",
+    appId: "1:88882724595:web:40b919b492d15f6a420186"
+};
+
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+
+const helpers = {
+    async getFormations(name:string ="", niveau:string ="", type:string =""){
+        let formationsList = await db.collection("Formation").get()
+        let formations: any[] = []
+        formationsList.forEach((formation) => {
+            let datas = formation.data()
+            console.log(name)
+            if(datas.name.indexOf(name) >= 0 || name == "")
+            formations.push({name:datas.name, lvl:datas.level, type:datas.type, id: formation.id})
+        })
+        console.log(JSON.stringify(formations))
+        console.log(JSON.parse(JSON.stringify(formations)))
+        return JSON.stringify(formations)
+    },
+}
+
+export default helpers
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useEffect, useState } from 'react';
+import { IonButton, IonCol, IonContent, IonGrid, IonHeader, IonIcon, IonPage, IonRow, IonSearchbar, IonTab, IonTitle, IonToolbar } from '@ionic/react';
+import helpers from '../helpers/helpers'
 import './Tab1.css';
-
+import { searchCircle } from 'ionicons/icons';
+let update = 0
 const Tab1: React.FC = () => {
+  const [searched, setSearched] = useState("")
+  const [formations, setFormations] = useState("[]")
+  useEffect(() => {
+    console.log("effect")
+    helpers.getFormations(searched).then((value) => {
+      setFormations(value)
+      console.log(formations)
+    })
+  }, [update, searched])
   return (
     <IonPage>
       <IonHeader>
@@ -17,7 +27,49 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonGrid>
+          <IonRow class="table" key="title-col">
+            <IonCol class="ion-text-center">
+              Formation
+            </IonCol>
+            <IonCol class="ion-text-center">
+              Niveau
+            </IonCol>
+            <IonCol class="ion-text-center">
+              Type
+            </IonCol>
+            <IonCol></IonCol>
+            <IonCol></IonCol>
+          </IonRow>
+          {JSON.parse(formations).map((formation:any) => {
+            return (
+              <IonRow class="table" key={formation.id }>
+                <IonCol class="ion-text-center">
+                  {formation.name}
+                </IonCol>
+                <IonCol class="ion-text-center">
+                  {formation.lvl}
+                </IonCol>
+                <IonCol class="ion-text-center">
+                  {formation.type}
+                </IonCol>
+                <IonCol class="ion-text-center">
+                  <IonButton onClick={() => {setFormations("[]"); update++}} fill="outline">Modifier</IonButton>
+                </IonCol>
+                <IonCol class="ion-text-center">
+                  <IonButton>Supprimer</IonButton>
+                </IonCol>
+              </IonRow>
+            )
+          })}
+          <IonRow class="ion-justify-content-center">
+            <IonCol size="8">
+              <IonSearchbar onIonChange={(e) => {setSearched(e.detail.value); console.log(e.detail.value)}} animated={true} placeholder="Search" />
+            </IonCol>
+            <IonCol size="2"><IonButton><IonIcon icon={searchCircle} /></IonButton></IonCol>
+          </IonRow>
+        </IonGrid>
+        {/* <ExploreContainer name="Tab 1 page" /> */}
       </IonContent>
     </IonPage>
   );
